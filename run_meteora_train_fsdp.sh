9dcc885a30614a54122383c244281068ecaef29f
Jingwei Xu <jingwei.xu.nju@gmail.com>
2024-06-12 16:07:00 +0800

    update fsdp sh

--- run_meteora_train_fsdp.sh
+++ run_meteora_train_fsdp.sh
@@ -1,7 +1,7 @@
-accelerate launch --num_processes=2 --config_file "configs/fsdp_config.yaml" train_old.py \
---model_name "/data1/model/llama2/meta-llama/Llama2-13b" \
+accelerate launch --num_processes=2 --config_file "configs/fsdp_config.yaml" meteora_train.py \
+--model_name "/data1/model/llama3/unsloth/Llama3-8b" \
 --tasks_datasets_prefix "/data0/ljy/workspace/BIG-bench/fuze_28_balance_no_sys/" \
---lora_path_prefix "/data0/ljy/workspace/LLaMA-Factory/ckpt/llama2_13b_fuze27_no_sys/" \
+--lora_path_prefix "/data0/ljy/workspace/LLaMA-Factory/ckpt/llama3_8b_fuze27_no_sys/" \
 --default_task "alpaca" \
 --max_seq_len 4096 \
 --max_steps 5000 \
@@ -10,12 +10,12 @@ accelerate launch --num_processes=2 --config_file "configs/fsdp_config.yaml" tra
 --save_steps 500 \
 --bf16 True \
 --packing True \
---output_dir "/data2/xjw/llama-meteor-data/test" \
+--output_dir "/data2/xjw/llama-meteor-data/train_gate_and_loras" \
 --per_device_train_batch_size 4 \
 --gradient_accumulation_steps 2 \
 --per_device_eval_batch_size 8 \
 --dataset_text_field "text" \
---learning_rate 7e-3  \
+--learning_rate 7e-4  \
 --lr_scheduler_type "cosine" \
 --weight_decay 0.01 \
 --warmup_ratio 0.05 \
